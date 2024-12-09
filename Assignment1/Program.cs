a200093ed992a6f1bca4e1291d412dc844d4709a
Daria <162370123+DStarodubrovskaya@users.noreply.github.com>
2024-12-09 23:14:02 +0200

    Added menu interface and enhanced class methods in Library Management System
    Added a user-friendly menu system to the Main method with options for managing the library.
    Implemented validation for user input in the menu to ensure robust operation.
    Fixed logical bug in the CheckTheList method to correctly iterate through subscribers.
    Updated LookOnTheShelf to simplify iteration using bookCount for efficiency.
    Improved code structure to prepare for modularized handling of menu options (e.g., adding books and subscribers).
    Added console output for better user interaction feedback.

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -20,7 +20,7 @@
             public static bool operator == (Book book1, Book book2)
             {
                 return ((book1.title == book2.title) && (book1.author == book2.author));
-            }
+            } 
             public static bool operator != (Book book1, Book book2)
             {
                 return !(book1 == book2); // negates the result of the == operator
@@ -62,34 +62,155 @@
             private int subscriberId = 0;
             private int booksOnLoan = 0;
             public Subscriber() { } // default constructor
+            public string SubscriberName { get { return subscriberName; } set { subscriberName = value; } }
+            public int SubscriberId { get { return subscriberId; } set { subscriberId = value; } }
+            public int BooksOnLoan { get { return booksOnLoan; } set { booksOnLoan = value; } }
         }
         class Library
         {
-            const int shelfLength = 5; // Перезаписать на 25!!
-            Book[] books = new Book[shelfLength];
-            Subscriber[] subscribers = new Subscriber[shelfLength];
+            private const int maxLength = 5; // Перезаписать на 25!!
+
+            private Book[] books = new Book[maxLength];
+            private int bookCount = 0; // tracks the number of books
+
+            Subscriber[] subscribers = new Subscriber[maxLength];
+            private int subCount = 0; // tracks the number of subscribers
             public Library() { } // default constructor
             public bool LookOnTheShelf(Book newBook)
             {
-                for (int i = 0; i < books.Length; i++)
+                for (int i = 0; i < bookCount; i++)
                 {
-                    if (books[i] != null) 
-                    {
-                        break; // stops searching if we hit an empty slot
-                    }
-                    if (books[i] == newBook)
+                    if ((books[i] == newBook))
                     {
                         return true; // in case: book found
                     }
                 }
                 return false; // in case: book not found
             }
+            public bool CheckTheList(Subscriber newSub)
+            {
+                for (int i = 0; i < subCount; i++)
+                {
+                    if ((subscribers[i].SubscriberId == newSub.SubscriberId))
+                    {
+                        return true; // in case: subscriber found
+                    }
+                }
+                return false; // in case: subscriber not found
+            }
+            public void AddBook(Book newBook)
+            {
+                if (LookOnTheShelf(newBook))
+                {
+                    Console.WriteLine("This book already exists in the library.");
+                }
+                else
+                {
+                    if (bookCount >= maxLength)
+                    {
+                        Console.WriteLine("Sorry, the library is full.");
+                    }
+                    else
+                    {
+                        books[bookCount] = newBook;
+                        bookCount++;
+                        Console.WriteLine("Success! New book was added to the library.");
+                    }
+                }
+            }
+            public void AddSubscruber(Subscriber newSub)
+            {
+                if (CheckTheList(newSub))
+                {
+                    Console.WriteLine("Subscriber exists in the system.");
+                }
+                else
+                {
+                    if (subCount >= maxLength)
+                    {
+                        Console.WriteLine("Sorry, our system is full.\nWe can't receive another subscriber.");
+                    }
+                    else
+                    {
+                        subscribers[subCount] = newSub;
+                        subCount++;
+                        Console.WriteLine("Success! New subscriber were added to the system.");
+                    }
+                }
+            }
         }
 
 
-            static void Main(string[] args)
+        static void Main(string[] args)
         {
+            Library library = new Library();
 
+            Console.WriteLine("Hello and welcome to our Library.");
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("\nWhat would you like to do?\n\n" +
+                    "Here is our menu:");
+                Console.WriteLine
+                    ("1 - Add a new book.\n" +
+                    "2 - Add a new subcriber press.\n" +
+                    "3 - Borrow a book.\n" +
+                    "4 - Return a book.\n" +
+                    "5 - Display all books.\n" +
+                    "6 - Display books by genre.\n" +
+                    "7 - Exit.");
+                Console.WriteLine("Enter your choice (1-7):");
+
+                int choice = -1;
+                while (true)
+                {
+                    while (!int.TryParse(Console.ReadLine(), out choice))
+                    {
+                        Console.WriteLine("Error, the value needs to be a whole number from 1 to 7.\n" +
+                            "Try again.");
+                    }
+                    if (choice >= 1 && choice <= 7)
+                    {
+                        Console.WriteLine($"You've entered {choice}.\nLet's proceed.\n");
+                        break;
+                    }
+                    else { Console.WriteLine("Error, the number needs to be from 1 to 7.\nTry again."); }
+                }
+                switch (choice)
+                {
+                    case 1: Console.WriteLine("\n\n - Adding a book. - \n\n");
+                        // Here will be a code
+                        break;
+                    case 2:
+                        Console.WriteLine("\n\n - Adding a subscriber. - \n\n");
+                        // Here will be a code
+                        break;
+                    case 3:
+                        Console.WriteLine("\n\n - Borrowing a book. - \n\n");
+                        // Here will be a code
+                        break;
+                    case 4:
+                        Console.WriteLine("\n\n - Returning a book. - \n\n");
+                        // Here will be a code
+                        break;
+                    case 5:
+                        Console.WriteLine("\n\n - Displaying all books. - \n\n");
+                        // Here will be a code
+                        break;
+                    case 6:
+                        Console.WriteLine("\n\n - Displaying books by genre. - \n\n");
+                        // Here will be a code
+                        break;
+                    case 7:
+                        Console.WriteLine("\n\n - Exiting the system. - \n\n");
+                        Console.WriteLine("Goodbye :)");
+                        exit = true;
+                        break;
+                }
+                Console.WriteLine("Press 'Enter' to continue.");
+                Console.ReadLine();
+            }
         }
     }
 }
